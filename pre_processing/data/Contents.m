bf4627e7530526d2ed6347b24dced7644d3680d1
SSutojo <99799060+SSutojo@users.noreply.github.com>
2022-06-02 09:22:24 +0100

    pre processing

---
+++ pre_processing/data/Contents.m
@@ -0,0 +1,67 @@
+% AMT - Data
+%
+%   Data from papers used to compare to model results
+%      DATA_BAUMGARTNER2017 - HRTFs, behavioral results and event-related potentials from Baumgartner et al. (2017), auditory looming study
+%      DATA_BEST2005        - Localization responses from Best et al. (2005), band-limited speech
+%      DATA_BOYD2012        - Externalization ratings from Boyd et al. (JASA-EL, 2012)
+%      DATA_BREEBAART2001   - Binaural masking level differences from Breebaart et al. (2001)      DATA_ELBERLING2010   - ABR wave V data as functon of level and sweeping rate.
+%      DATA_ELBERLING2010    - ABR Wave V Data
+%      DATA_GLASBERG1990    - Notched noise thresholds.
+%      DATA_GLASBERG2002    - Weights used in loudness models Moore et al. (1997) and Glasberg and Moore (2002)
+%      DATA_GOODE1994       - Stapes footplate diplacement.
+%      DATA_GOUPELL2010     - Localization responses from Goupell et al. (2010), number of spectral channels
+%      DATA_GREEN1964       - Converts the overall SNRenv to percent correct.
+%      DATA_HARTE2009       - Tone burst stimuli.
+%      DATA_HARTMANN1996    - Externalization ratings from Hartmann and Wittenberg (1996) 
+%      DATA_HASSAGER2016    - Externalization ratings from Hassager et al. (JASA, 2016)
+%      DATA_JOERGENSEN2011  - Change in SRT measured in Joergensen and Dau (2011).
+%      DATA_LANGENDIJK2002  - Localization responses from Langendijk et al. (2002)
+%      DATA_LINDEMANN1986   - Lindemann et al. (1986).
+%      DATA_LYON2011        - Lyon et al. (2011)
+%      DATA_MACPHERSON2003  - Localization responses from Macpherson and Middlebrooks (2003), spectral ripples
+%      DATA_MAJDAK2010      - Localization responses from Majdak et al. (2010), general localization
+%      DATA_MAJDAK2013      - Localization responses from Majdak et al. (2013), spectral warping study
+%      DATA_MAJDAK2013CTC   - Localization responses from Majdak et al. (2013), cross-talk cancellation study
+%      DATA_MIDDLEBROOKS1999 - Localization responses for individualized and non-ind. HRTFs from Middlebrooks (1999)
+%      DATA_NEELY1988       - ABR wave V latency as function of center frequency.
+%      DATA_PRALONG1996     - Headphone and outer ear data.
+%      DATA_PULKKI2001      - Data for sound localization in median plane from Pulkki et al. (2001)
+%      DATA_SABIN2005       - Localization responses from Figures 8-10 of Sabin et al. (2005)
+%      DATA_VANDEPAR1999    - Data points from van de Par and Kohlrausch (1999)
+%      DATA_VANDEPAR1999    - N0S0 masking thresholds from van de Paar et al. (1999)
+%      DATA_ZWICKER1961     - Definition of critical bands.
+%
+%   Data used for model simulations
+%      DATA_BAUMGARTNER2013 - Data from Baumgartner et al. (2013)
+%      DATA_BAUMGARTNER2014  - Data from Baumgartner et al. (2014)
+%      DATA_BAUMGARTNER2016  - Data from Baumgartner et al. (2016)
+%      DATA_BAUMGARTNER2017LOOMING - HRTFs from Baumgartner et al. (2017)
+%      DATA_BRIMIJOIN2013  - Data from Brimijoin et al. (2013)
+%      DATA_LOPEZPOVEDA2001 - Outer and middle ear filter data.
+%      DATA_MACPHERSON2003  - Data from MacPherson et al. (2003)
+%      DATA_VLIEGEN2004  - Data from Vliegen et al. (2004)
+%      DATA_ROENNE2012      - Unitary response.
+%      DATA_TAKANEN2013     - Data from Takanen et al. (2013)
+%      DATA_WIERSTORF2013   - Data from Wierstorf et al. (2013)
+%      DATA_ZIEGELWANGER2013 - Time-of-arrival in HRTFs from Ziegelwanger et al. (2013)
+%      DATA_ZIEGELWANGER2014 - Time-of-arrival in HRTFs from Ziegelwanger and Majdak (2014)
+%      DATA_LI2020           - Data from Li et al. (2020)
+%
+%   Url: http://amtoolbox.org/amt-1.1.0/doc/data/Contents.php
+
+% Copyright (C) 2009-2021 Piotr Majdak, Clara Hollomey, and the AMT team.
+% This file is part of Auditory Modeling Toolbox (AMT) version 1.1.0
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
