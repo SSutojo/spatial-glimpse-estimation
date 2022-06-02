35e9f549f1952b39b7edc353d841ce2551b13999
SSutojo <99799060+SSutojo@users.noreply.github.com>
2022-06-02 09:33:03 +0100

    pre processing

---
+++ pre_processing/amtoolbox/legacy/test_exp_osses2022.m
@@ -0,0 +1,46 @@
+% exp_bruce2018('fig7b');
+% exp_bruce2018('fig8b');
+% exp_bruce2018('fig10');
+%
+%   Url: http://amtoolbox.org/amt-1.1.0/doc/legacy/test_exp_osses2022.php
+
+% Copyright (C) 2009-2021 Piotr Majdak, Clara Hollomey, and the AMT team.
+% This file is part of Auditory Modeling Toolbox (AMT) version 1.1.0
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+models=[1 2 3 4 5 6 7 8];
+%     'dau1997'           : Model #1 from Dau et al., (1997)
+%     'zilany2014'        : Model #2 from Zilany et al., (2014)
+%     'verhulst2015'      : Model #3 from Verhulst et al., (2015)
+%     'verhulst2018'      : Model #4 from Verhulst et al., (2018)
+%     'bruce2018'         : Model #5 from Bruce et al., (2018)
+%     'king2019'          : Model #6 from King et al., (2019)
+%     'relanoiborra2019'  : Model #7 from Relano-Iborra et al., (2019)
+%     'osses2021'         : Model #8 from Osses and Kohlrausch (2021)
+exp_osses2022('fig3','models',models);
+exp_osses2022('fig4','models',models);
+exp_osses2022('fig5','models',models);
+exp_osses2022('fig6','models',models);
+exp_osses2022('fig7','models',models);
+exp_osses2022('fig8','models',models);
+exp_osses2022('fig9','models',models);
+exp_osses2022('fig10','models',models);
+exp_osses2022('fig11','models',models);
+exp_osses2022('fig12a','models',models);
+exp_osses2022('fig12b','models',models);
+exp_osses2022('fig13','models',models);
+exp_osses2022('fig14a','models',models);
+exp_osses2022('fig14b','models',models);
+exp_osses2022('fig15','models',models);
+
